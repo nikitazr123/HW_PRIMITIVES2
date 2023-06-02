5f2581dfd379566b24d294a93e11aa126f8a2f5e
Nik test <nik.zyryanov.01@gmail.com>
2023-06-02 21:01:58 +0300

    HWPR

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
         int bonus;
 //        System.out.println("Счёт после пополнения: "+ (finalClientBalance));
 
-        if (upBalance >= 1000) {
+        if (upBalance > 1000) {
             bonus = (int) upBalance / 100;
-            System.out.println("Полученный бонус: "+ (bonus));
-            System.out.println("Ваш счёт равен: "+ (bonus + clientBalance + upBalance));
+            System.out.println("Полученный бонус: " + (bonus));
+            System.out.println("Ваш счёт равен: " + (bonus + clientBalance + upBalance));
         } else {
-            System.out.println("Ваш счёт равен: "+ (clientBalance + upBalance));
-            System.out.println("Полученный бонус: "+ ( bonus = 0) +" Для получения бонусов, пожалуйста пополниете счёт более чем на 1000р");
+            System.out.println("Ваш счёт равен: " + (clientBalance + upBalance));
+            System.out.println("Полученный бонус: " + (bonus = 0) + " Для получения бонусов, пожалуйста пополниете счёт более чем на 1000р");
         }
     }
 }
